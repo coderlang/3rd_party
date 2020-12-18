a77b40310f7518bca956704c23812e0e47f786cc
coderlang <coderlang@foxmail.com>
2020-12-18 16:06:24 +0800

    修复bug

--- script/cmd/init.js
+++ script/cmd/init.js
@@ -1,7 +1,8 @@
+let pwd=process.cwd();
+
 let git = require(`${pwd}/3rd_party/script/git.js`);
 git.checkHEADDetached();
 
-let pwd=process.cwd();
 require(`${pwd}/3rd_party/script/cmd/preinstall.js`);
 console.log("然后执行 npm install");
 
